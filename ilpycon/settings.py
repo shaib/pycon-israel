3a52afce5bb4dd46225ca5ef1075d3c33530d612
Shai Berger <shai@platonix.com>
2018-04-15 23:39:48 +0300

    Add external domains as trusted CSRF origins

--- ilpycon/settings.py
+++ ilpycon/settings.py
@@ -19,6 +19,11 @@ ALLOWED_HOSTS = [
     "localhost",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "il.pycon.org",
+    "pycon.org.il",
+]
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
